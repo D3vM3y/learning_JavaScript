d9353a65a1dd37eb184d0924adf60b3522b1a2ec
Tarik Mey <tarik.mey@gmail.com>
2023-06-12 07:28:15 +0200

    added tasks to practice my knowledge on js variabels let, const, and var.

--- js_projects/js_variables/js_file.js
+++ js_projects/js_variables/js_file.js
@@ -1,7 +1,23 @@
-let age = 25;
-let firstName = "John";
-let job = "cashier";
+"use strict";
 
-let message = firstName + " is " + age + " years old and works as a " + job + ".";
+// written in capital letters and using underscore because I use a constant as an alias for a difficult-to-remember value, which is known prior the code execution
+const COLOR_ORANGE = "#FF7F00";
 
-console.log(message);
+
+// tasks to train your knowledge on variables
+// Working with variables
+// 1. Declare two variables: admin and firstName.
+let admin;
+let firstName;
+// 2. Assign the value "John" to firstName.
+firstName = "John";
+// 3. Copy the value from firstName to admin.
+admin = firstName;
+// 4. Show the value of admin using alert (must output “John”).
+alert("Admin: " + admin);
+
+// Giving the right name
+// 1. Create a variable with the name of our planet. How would you name such a variable?
+let planetEarth;
+// 2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+let currentUser;
