486ff26050e5d516f6744d9454df0bbf67f7bced
Tarik Mey <tarik.mey@gmail.com>
2023-06-12 07:32:23 +0200

    created new files for data types module

---
+++ js_projects/js_dataTypes/js_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";
+// =================
+// === Variables ===
+// written in capital letters and using underscore because I use a constant as an alias for a difficult-to-remember value, which is known prior the code execution
+const COLOR_ORANGE = "#FF7F00";
+
+
+// tasks to train your knowledge on variables
+// Working with variables
+// 1. Declare two variables: admin and firstName.
+let admin;
+let firstName;
+// 2. Assign the value "John" to firstName.
+firstName = "John";
+// 3. Copy the value from firstName to admin.
+admin = firstName;
+// 4. Show the value of admin using alert (must output “John”).
+alert("Admin: " + admin);
+
+// Giving the right name
+// 1. Create a variable with the name of our planet. How would you name such a variable?
+let planetEarth;
+// 2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+let currentUser;
