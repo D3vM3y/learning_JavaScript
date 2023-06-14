bca2d793edb383983c529e2452b4c24abf57a90b
Tarik Mey <tarik.mey@gmail.com>
2023-06-14 06:14:55 +0200

    adding arrays to knowledge database

--- js_projects/js_basics/js_basics.js
+++ js_projects/js_basics/js_basics.js
@@ -17,18 +17,31 @@ let isMember = true;
         isMember,
     }
 
-    
+// Arrays
+    // create array to hold multiple users
+    let users = [];
+
     
 // functions
-    //populate the object keys with values
+    //basic function to populate the object keys with values
     function addUserInfo(firstName, lastName, id, email, phone, isMember){
-        user.firstName = this.firstName;
-        user.lastName = this.lastName;
-        user.id = this.id;
-        user.email = this.email;
-        user.phone = this.phone;
-        user.isMember = this.isMember;
+        user.firstName = firstName;
+        user.lastName = lastName;
+        user.id = id;
+        user.email = email;
+        user.phone = phone;
+        user.isMember = isMember;
     }
+
+    // adding single user to array users
+    function addUserToUsers(users, newUser){
+        
+    }
+
+
+    // calling the addUserInfo function to populate the user object
+    addUserInfo(firstName,lastName,id,email,phone,isMember);
+
 // classes
 // loops
 // Modules
