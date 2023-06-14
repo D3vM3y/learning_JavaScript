c6c733565aed3c555097bc9eaf08d061e876bd65
Tarik Mey <tarik.mey@gmail.com>
2023-06-14 06:28:35 +0200

    adding users array and adding new users to that array

--- js_projects/js_basics/js_basics.js
+++ js_projects/js_basics/js_basics.js
@@ -9,38 +9,50 @@ let isMember = true;
 // objects
     // create basic object
     let user = {
-        firstName,
-        lastName,
-        id,
-        email,
-        phone,
-        isMember,
+        firstName: firstName,
+        lastName: lastName,
+        id: id,
+        email: email,
+        phone: phone,
+        isMember: isMember,
     }
 
 // Arrays
     // create array to hold multiple users
     let users = [];
 
+    // adding the user to the users array
+    users[0] = user;
     
 // functions
     //basic function to populate the object keys with values
     function addUserInfo(firstName, lastName, id, email, phone, isMember){
-        user.firstName = firstName;
-        user.lastName = lastName;
-        user.id = id;
-        user.email = email;
-        user.phone = phone;
-        user.isMember = isMember;
+        let newUser = {
+        firstName: firstName,
+        lastName: lastName,
+        id: id,
+        email: email,
+        phone: phone,
+        isMember: isMember,
+        };
+
+        return newUser;
     }
 
     // adding single user to array users
     function addUserToUsers(users, newUser){
-        
+        users[users.length] = newUser;
     }
 
 
     // calling the addUserInfo function to populate the user object
     addUserInfo(firstName,lastName,id,email,phone,isMember);
+
+    //adding a new user to the database array users
+    addUserToUsers(users, addUserInfo("Sandra", "Miller", "2789", "sandra.miller@gmail.com", "+7823197804"));
+    addUserToUsers(users, addUserInfo("Richard", "Miller", "2790", "richard.miller@gmail.com", "+7823197804"));
+
+    console.log(users);
 
 // classes
 // loops
