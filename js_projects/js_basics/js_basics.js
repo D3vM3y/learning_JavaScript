acfd4264757b9350d71382a00c8a361291d4fe8e
Tarik Mey <tarik.mey@gmail.com>
2023-06-18 16:27:42 +0200

    adding example for getters and setters in classes.

--- js_projects/js_basics/js_basics.js
+++ js_projects/js_basics/js_basics.js
@@ -357,6 +357,34 @@ let isMember = true;
     //   let user = new User("John");
     //   user.sayHi();
 
+    // Getters/Setters
+    // Example
+    // class User {
+
+    //     constructor(name) {
+    //       // invokes the setter
+    //       this.name = name;
+    //     }
+      
+    //     get name() {
+    //       return this._name;
+    //     }
+      
+    //     set name(value) {
+    //       if (value.length < 4) {
+    //         alert("Name is too short.");
+    //         return;
+    //       }
+    //       this._name = value;
+    //     }
+      
+    //   }
+      
+    //   let user = new User("John");
+    //   alert(user.name); // John
+      
+    //   user = new User(""); // Name is too short.
+
 // Modules
     // https://javascript.info/modules
 // Promises, async/await
