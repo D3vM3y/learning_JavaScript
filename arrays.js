d8d3b695e4cab0a121351f5da123194605129d14
Tarik Mey <tarik.mey@gmail.com>
2023-06-24 18:38:21 +0200

    creating arrays.js for storing information about js arrays

---
+++ arrays.js
@@ -0,0 +1,19 @@
+const data = [];
+
+const dataPoint = {
+    name: "Jack",
+    age: 21,
+    member: true,
+}
+
+data.push(dataPoint);
+data.push({name: "Henry", age: 34, member: false});
+data.push({name: "Sarah", age: 25, member: true});
+
+// map
+const memberDetails = (memberDetail) => {
+    data.map();
+}
+// filter
+// reduce
+console.log(data);
