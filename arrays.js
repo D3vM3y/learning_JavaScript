e7353491b1e77840310d07e7c9aecb1301ef0c4a
Tarik Mey <tarik.mey@gmail.com>
2023-06-24 18:56:00 +0200

    adding map function to iterate over the allClients array

--- arrays.js
+++ arrays.js
@@ -2,30 +2,29 @@ const allClients = [];
 const members = [];
 const potentialMembers = [];
 
-
+// creating simple member object
 const client = {
     name: "Jack",
     age: 21,
     member: true,
 }
 
+// pushing data to the allClients array
 allClients.push(client);
 allClients.push({name: "Henry", age: 34, member: false});
 allClients.push({name: "Sarah", age: 25, member: true});
 
 // map
 const isMember = allClients.map((memberDetail) => {
-    let member = memberDetail.member === true && memberDetail;
-    return member;
+    return memberDetail.member === true && members.push(memberDetail);
 })
 
 const interestedClient = allClients.map((memberDetail) => {
-    let potentialMember = memberDetail.member === false && memberDetail;
-    return potentialMember;
+    return memberDetail.member === false && potentialMembers.push(memberDetail);
 })
+
 // filter
 // reduce
 
-// pushing data to the arrays
 
-console.log();
+console.log(members);
