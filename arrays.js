5af4f742676fedb9705977acfe8c7ba369c24393
Tarik Mey <tarik.mey@gmail.com>
2023-06-24 18:50:24 +0200

    adding map function to map over the allClients array

--- arrays.js
+++ arrays.js
@@ -1,19 +1,31 @@
-const data = [];
+const allClients = [];
+const members = [];
+const potentialMembers = [];
 
-const dataPoint = {
+
+const client = {
     name: "Jack",
     age: 21,
     member: true,
 }
 
-data.push(dataPoint);
-data.push({name: "Henry", age: 34, member: false});
-data.push({name: "Sarah", age: 25, member: true});
+allClients.push(client);
+allClients.push({name: "Henry", age: 34, member: false});
+allClients.push({name: "Sarah", age: 25, member: true});
 
 // map
-const memberDetails = (memberDetail) => {
-    data.map();
-}
+const isMember = allClients.map((memberDetail) => {
+    let member = memberDetail.member === true && memberDetail;
+    return member;
+})
+
+const interestedClient = allClients.map((memberDetail) => {
+    let potentialMember = memberDetail.member === false && memberDetail;
+    return potentialMember;
+})
 // filter
 // reduce
-console.log(data);
+
+// pushing data to the arrays
+
+console.log();
